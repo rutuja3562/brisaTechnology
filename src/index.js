9eef44ba1fdccc391211a14c87661ada95334468
Rutuja Patil <rspatil3562@gmail.com>
2023-02-15 11:11:47 +0530

    Done

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import App from "./App";
 
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
-import { GoogleOAuthProvider } from "@react-oauth/google";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
